6efc9d33591915749b280eb540cb0e307ee1ab64
lgdd <louis-guillaume.durand@liferay.com>
2020-03-04 17:49:28 +0100

    Improve handling loading, error, and empty list

--- modules/lfug-react-app/src/components/BlogList.js
+++ modules/lfug-react-app/src/components/BlogList.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Blog from './Blog';
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
+import ClayLoadingIndicator from '@clayui/loading-indicator';
+import ClayAlert from '@clayui/alert';
 
 const SITE_ID = 20124;
 
@@ -26,8 +28,25 @@ const ALL_BLOGS = gql`
 export default function BlogList() {
   const { loading, error, data } = useQuery(ALL_BLOGS);
 
-  if (loading) return 'Loading...';
-  if (error) return `Error! ${error.message}`;
+  if (loading) return <ClayLoadingIndicator />;
+  if (error) {
+    return (
+      <ClayAlert displayType="danger" title="Error:">
+        An error occured while loading data.
+        <div className="mt-2">
+          <code>{error.message}</code>
+        </div>
+      </ClayAlert>
+    );
+  }
+
+  if (data.blogPostings.items.length === 0) {
+    return (
+      <ClayAlert displayType="info" className="text-center">
+        No blog posts.
+      </ClayAlert>
+    );
+  }
 
   const blogs = data.blogPostings.items.map(
     ({ id, headline, articleBody, creator }) => (

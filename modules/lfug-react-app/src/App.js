a6dd1cde0e91fd0ce653ce277b6b7f80709f85db
lgdd <louis-guillaume.durand@liferay.com>
2020-03-04 17:26:59 +0100

    Create React app & cleanup some files

---
+++ modules/lfug-react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="lfug-react-app">
+      <header>
+        <h1>Hello, world!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;

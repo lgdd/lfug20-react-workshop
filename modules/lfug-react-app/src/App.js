46cab7431b2accd102387fa387b7f88b905c07f0
lgdd <louis-guillaume.durand@liferay.com>
2020-03-04 17:45:24 +0100

    Add Apollo Client & Provider

--- modules/lfug-react-app/src/App.js
+++ modules/lfug-react-app/src/App.js
@@ -1,18 +1,47 @@
 import React from 'react';
 import './css/App.css';
-
 import '@clayui/css/lib/css/atlas.css';
 import BlogList from './components/BlogList';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+// Don't forget to create a file '.env.local'
+// and to add keys & values that we're using in this app
+// according to your environment.
+import 'dotenv/config';
 
-function App() {
+export default function App() {
   return (
-    <div className="lfug-react-app">
-      <div className="container">
-        <h1 className="text-center mb-4">Blogs</h1>
-        <BlogList />
+    <ApolloProvider client={createApolloClient()}>
+      <div className="lfug-react-app">
+        <div className="container">
+          <h1 className="text-center mb-4">Blogs</h1>
+          <BlogList />
+        </div>
       </div>
-    </div>
+    </ApolloProvider>
   );
 }
 
-export default App;
+function createApolloClient() {
+  let endpoint;
+  const user = process.env.REACT_APP_LIFERAY_USER;
+  const password = process.env.REACT_APP_LIFERAY_PASSWORD;
+  const base64credentials = new Buffer(`${user}:${password}`).toString(
+    'base64'
+  );
+
+  endpoint =
+    process.env.REACT_APP_LIFERAY_HOST +
+    process.env.REACT_APP_LIFERAY_GRAPHQL_ENDPOINT;
+
+  return new ApolloClient({
+    uri: endpoint,
+    headers: {
+      Authorization: 'Basic ' + base64credentials
+    }
+  });
+}
+
+export function Liferay() {
+  return window['Liferay'];
+}
